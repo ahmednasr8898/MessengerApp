581849f1c83c5c57bc1a13a3b4dc3cca0da6ba4e
ahmednasr8898 <ans96283@gmail.com>
2021-05-10 04:16:19 +0200

    ChangeBackgroundColorForProfilePic

--- MessengerApp/Controllers/ProfileViewController.swift
+++ MessengerApp/Controllers/ProfileViewController.swift
@@ -34,7 +34,7 @@ class ProfileViewController: UIViewController {
         let path = "images/"+fileName //images -> safeEmail_profile_picture.png
         
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.width, height: 300))
-        headerView.backgroundColor = .blue
+        headerView.backgroundColor = .gray
         let profileImage = UIImageView(frame: CGRect(x: (headerView.width - 150) / 2, y: 75, width: 150, height: 150))
         profileImage.contentMode = .scaleAspectFill
         profileImage.backgroundColor = .white
